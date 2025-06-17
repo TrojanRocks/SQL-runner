b4b15a74b9314273813316fd4601c0656d0bde5e
Tarun Rai Tara <taruntara93@gmail.com>
2025-06-17 11:43:09 +0530

    Fix build failure

--- src/service-worker.js
+++ src/service-worker.js
@@ -63,10 +63,12 @@ registerRoute(
 
 // This allows the web app to trigger skipWaiting via
 // registration.waiting.postMessage({type: 'SKIP_WAITING'})
-self.addEventListener('message', (event) => {
+self.addEventListener('message', event => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
   }
 });
 
-// Any other custom service worker logic can go here.
+addEventListener('fetch', event => {
+  // Any other custom service worker logic can go here.
+});
